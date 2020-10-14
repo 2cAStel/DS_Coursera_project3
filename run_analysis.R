1e6b0d9d7b3f5403b4a93d4b33d4ff74e2819536
alaisebzh <arthurdecastel@live.fr>
2020-10-14 16:37:47 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,7 +5,7 @@ library(dplyr)
 filename <- "Coursera_project_S3.zip"
 foldername <- './projectsequence3'
 
-##setwd(foldername)
+setwd(foldername)
 
 if (!file.exists(filename)){
 	fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
